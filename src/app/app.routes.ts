85ffbaa218880c69612255405bc601a6aecd0480
anej.bradac <brad.vran.anej@gmail.com>
2024-07-17 14:07:14 +0200

    Added routes and navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent,
-    }
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  { path: 'about', title: 'App About Page', component: AboutComponent },
 ];
